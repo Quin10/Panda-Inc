f65ebd0c62f4e3ed8b1979911afb85b0c3e00077
Keaton <keatonmatthewp@hotmail.com>
2017-07-04 21:47:31 +0200

    Added main body

--- application/app/src/main/MultiThreadedServer.java
+++ application/app/src/main/MultiThreadedServer.java
@@ -1,1 +1,39 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
 
+public class MultiThreadedServer implements Runnable{
+
+    protected int          serverPort   = 8080;
+    protected ServerSocket serverSocket = null;
+    protected boolean      isStopped    = false;
+    protected Thread       runningThread= null;
+
+    public MultiThreadedServer(int port){
+        this.serverPort = port;
+    }
+
+    public void run(){
+        synchronized(this){
+            this.runningThread = Thread.currentThread();
+        }
+        openServerSocket();
+        while(! isStopped()){
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if(isStopped()) {
+                    System.out.println("Server Stopped.") ;
+                    return;
+                }
+                throw new RuntimeException(
+                    "Error accepting client connection", e);
+            }
+            new Thread(
+                new WorkerRunnable(
+                    clientSocket, "Multithreaded Server")
+            ).start();
+        }
+        System.out.println("Server Stopped.") ;
+    }
